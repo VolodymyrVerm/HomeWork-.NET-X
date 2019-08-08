39b8340ab82847eb2e6a1a7199656b6fc8b5a8cc
VolodymyrVerm <volodymyr33929@gmail.com>
2019-08-08 15:03:01 +0300

    Task2

--- HomeWork .NET X/HomeWork .NET X/Program.cs
+++ HomeWork .NET X/HomeWork .NET X/Program.cs
@@ -7,19 +7,29 @@ namespace HomeWork_.NET_X
     {
         static void Main(string[] args)
         {
+            Rectangle rect = new Rectangle();
+
             Console.WriteLine("Please input Left_X: ");
             double L_X = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("Please input Left_Y: ");
             double L_Y = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("Please input Right_X: ");
             double R_X = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("Please input Right_Y: ");
             double R_Y = Convert.ToDouble(Console.ReadLine());
-            Rectangle rect = new Rectangle(L_X, L_Y, R_X, R_Y);
+
+            rect.left_up_X = L_X;
+
+            rect.left_up_Y = L_Y;
+
+            rect.right_down_X = R_X;
+
+            rect.right_down_Y = R_Y;
 
             Console.WriteLine("Sqaure: {0}; Perimetr: {1};", rect.GetSqaure(), rect.GetPerimetr());
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
--- HomeWork .NET X/HomeWork .NET X/Rectangle.cs
+++ HomeWork .NET X/HomeWork .NET X/Rectangle.cs
@@ -6,21 +6,14 @@ namespace HomeWork_.NET_X
 {
     class Rectangle
     {
-        private double left_up_X;
+        public double left_up_X { get; set; }
 
-        private double left_up_Y;
+        public double left_up_Y { get; set; }
 
-        private double right_down_X;
+        public double right_down_X { get; set; }
 
-        private double right_down_Y;
+        public double right_down_Y { get; set; }
 
-        public Rectangle (double left_up_X, double left_up_Y, double right_down_X, double right_down_Y)
-        {
-            this.left_up_X = left_up_X;
-            this.left_up_Y = left_up_Y;
-            this.right_down_X = right_down_X;
-            this.right_down_Y = right_down_Y;
-        }
 
         public double GetSqaure()
         {
