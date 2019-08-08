464e8fb71b6e131b63fd71e8930011fb5cc12cc0
VolodymyrVerm <volodymyr33929@gmail.com>
2019-08-08 12:19:46 +0300

    Init commit

---
+++ HomeWork .NET X/HomeWork .NET X/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork_.NET_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
